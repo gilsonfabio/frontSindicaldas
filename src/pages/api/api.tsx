19c310511e71e47c4798e956092f3eb65e60e4ff
Gilson Fábio <gilsonfabio@gmail.com>
2023-05-22 11:34:13 -0300

    Vercel file

--- src/pages/api/api.tsx
+++ src/pages/api/api.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = 
-  axios.create({baseURL: 'https://backend-sindicaldas.herokuapp.com/' });
+  axios.create({baseURL: 'https://backcaldascard-i9dnxrve2-gilsonfabio.vercel.app/' });
+  //axios.create({baseURL: 'https://backend-sindicaldas.herokuapp.com/' });
   //axios.create({baseURL: 'http://localhost:3333' });
 
 export default api;
