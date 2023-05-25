fea1a0f9138ec2822f8bbc213b03d2fbb8880288
Gilson Fábio <gilsonfabio@gmail.com>
2023-05-25 12:55:52 -0300

    Alteração da api de acesso ao back

--- src/pages/api/api.tsx
+++ src/pages/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = 
-  axios.create({baseURL: 'https://backcaldascard-i9dnxrve2-gilsonfabio.vercel.app/' });
+  axios.create({baseURL: 'https://backcaldascard.vercel.app/' });
   //axios.create({baseURL: 'https://backend-sindicaldas.herokuapp.com/' });
   //axios.create({baseURL: 'http://localhost:3333' });
 
